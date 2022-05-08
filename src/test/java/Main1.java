4dd4198719d713221fe3a1297ff74dd235f4755e
Romanussss <vladroman1257@gmail.com>
2022-05-08 14:29:11 +0300

    TEMA

---
+++ src/test/java/Main1.java
@@ -0,0 +1,16 @@
+
+public class Main1 {
+
+    public static void main(String[] args) {
+        LoginTest login = new LoginTest();
+//        login.loginWithValidCredentialsTest();
+//        login.loginWithInvalidEmail();
+//        login.loginWithInvalidPasswordTest();
+//        login.loginWithoutCredentialsTest();
+        RegisterTest register = new RegisterTest();
+        register.registerCreditendials();
+
+
+    }
+
+}
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,13 +1,36 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.Driver;
+
 public class RegisterTest {
 
-    public void validRegisterTest(){
-        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+    public void validRegisterTest() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
 
     }
+
+    public void registerCreditendials() {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Roman");
+        driver.findElement(By.id("middlename")).sendKeys("O");
+        driver.findElement(By.id("lastname")).sendKeys("Vlad");
+        driver.findElement(By.cssSelector("#email_address")).sendKeys("vladhodisan@yahoo.com");
+        driver.findElement(By.cssSelector("#password")).sendKeys("1234567");
+        driver.findElement(By.cssSelector("#confirmation")).sendKeys("1234567");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span"));
+
+
+    }
+
+
 }
